8843539d3887f9122b912c9ed5dbf8d853343eba
Lucas <lucas.souza@edulabzz.com.br>
2023-06-04 18:37:45 -0300

    fix: memory

---
+++ aprendaRust04/proj/README.md
@@ -0,0 +1,16 @@
+static variables
+
+```rs
+static _Y: u32 = 13;
+
+fn main() {
+    println!("{}", _Y);
+}
+
+```
+
+will alocate on start program
+
+STACK
+
+any variable function on thread will use
---
+++ aprendaRust04/proj/target/debug/proj.d
@@ -0,0 +1,1 @@
+/home/lucasp/Documents/Projects/MyProjects/Rust/rust-study/aprendaRust04/proj/target/debug/proj: /home/lucasp/Documents/Projects/MyProjects/Rust/rust-study/aprendaRust04/proj/src/main.rs
